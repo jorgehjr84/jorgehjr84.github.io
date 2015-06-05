0ca54a79760d97e38c778729d5c100f3dd2248db
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2015-06-05 09:00:28 -0400

    Starting on the click event for arrow icon

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-// (function(window) {
-//
-// $('.arrow').click('')
-//
-//
-//
-//
-//
-//
-//
-// })(window);
+(function(window) {
+
+  $(document).ready(function() {
+
+
+
+
+    $('.arrow').click(function(scrollDown) {
+
+      $('#posts-page').animate({
+
+
+
+      }, 1000);
+
+    })
+
+
+
+  }); //End of document ready function
+
+
+
+})(window);
